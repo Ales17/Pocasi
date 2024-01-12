b1ce040ea5dcce48914edd6a5902503cf637938b
Ales Bacul <alesbacul@gmail.com>
2024-01-12 18:52:33 +0100

    refactor

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // Index of selected place for weather
 let placeIndex = 0;
 // Function to get url based by selected place
-function getApiUrl(currentIndex) {
+function getIndexApiUrl(currentIndex) {
   let lat = places[currentIndex].lat;
   let lon = places[currentIndex].lon;
+  return getApiUrlByCoords(lat, lon);
+}
+
+function getApiUrlByCoords(lat, lon) {
   return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&timezone=Europe%2FBerlin`;
 }
+
 // HTML catch elements
-const divWeather = document.querySelector("#div-weather");
+const divWeather = document.getElementById("div-weather");
 const overlay = document.getElementById("overlay");
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn")
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
 // Display cities in the page
 for (let i = 0; i < places.length; i++) {
   let cityBtn = document.createElement("button");
@@ -20,21 +25,21 @@ for (let i = 0; i < places.length; i++) {
   overlay.append(cityBtn);
   cityBtn.addEventListener("click", function (e) {
     placeIndex = e.target.id;
-    loadApi(getApiUrl(placeIndex));
-    toggle()
+    loadApi(getIndexApiUrl(placeIndex));
+    toggleMenuVisibility();
   });
 }
-closeBtn.addEventListener("click", () => toggle())
-menuBtn.addEventListener("click", () => toggle());
+closeBtn.addEventListener("click", () => toggleMenuVisibility());
+menuBtn.addEventListener("click", () => toggleMenuVisibility());
 
-let toggle = () =>
+let toggleMenuVisibility = () =>
   overlay.style.display == "none"
     ? (overlay.style.display = "grid")
     : (overlay.style.display = "none");
 
 function switchCity() {
   placeIndex === places.length - 1 ? (placeIndex = 0) : placeIndex++;
-  loadApi(getApiUrl(placeIndex));
+  loadApi(getIndexApiUrl(placeIndex));
 }
 
 const tempDiv = document.createElement("div");
@@ -49,28 +54,30 @@ divWeather.append(placeDiv, iconDiv, tempDiv);
 let getDetails = (weatherCode) =>
   weatherCodes.filter((e) => e.code === weatherCode);
 
-// Function - return icon element
-function getIcon(weatherCode) {
-  let weatherDetails = getDetails(weatherCode);
-  let iconName = weatherDetails[0].icon;
-  let desc = weatherDetails[0].description;
+function getWeatherIconElement(iconName, weatherDescription) {
   let iconElement = document.createElement("i");
   iconElement.classList.add("bi", `bi-${iconName}`);
-  iconElement.title = desc;
+  iconElement.title = weatherDescription;
   return iconElement.outerHTML;
+}
+ 
+function getWeatherIconInfo(weatherCode) {
+  let weatherDetails = getDetails(weatherCode);
+  let iconName = weatherDetails[0].icon;
+  let weatherDescription = weatherDetails[0].description;
+
+  return getWeatherIconElement(iconName, weatherDescription);
 }
 
 function renderFunkce(apiObject) {
-  console.log("RENDERING")
+  console.log("RENDERING");
   let temp = apiObject.current_weather.temperature;
   let code = apiObject.current_weather.weathercode;
   const place = places[placeIndex].city;
-  console.log(temp)
-  console.log(code)
+  console.log(`TEMP - ${temp}, CODE - ${code}`);
   tempDiv.textContent = `${temp} °C`;
   placeDiv.textContent = place;
-  iconDiv.innerHTML = getIcon(code);
-  
+  iconDiv.innerHTML = getWeatherIconInfo(code);
 }
 
 async function loadApi(url) {
@@ -80,6 +87,6 @@ async function loadApi(url) {
 }
 
 window.addEventListener("load", function () {
-  loadApi(getApiUrl(placeIndex));
+  loadApi(getIndexApiUrl(placeIndex));
 });
-setInterval(() => loadApi(getApiUrl(placeIndex)), 10000);
+setInterval(() => loadApi(getIndexApiUrl(placeIndex)), 10000);
